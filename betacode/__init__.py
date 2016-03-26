ae76cbfe89225c1bacc23246014a758030df1a78
Aaron Christianson <ninjaaron@gmail.com>
2016-03-26 18:57:47 -0500

    updated doc string

--- betacode/__init__.py
+++ betacode/__init__.py
@@ -1,5 +1,11 @@
 '''
-Helpers for importing 
+Helpers for decoding Greek and Hebrew betacode, such as one finds in
+CATSS, TLG, Perseus Tufts, and other weird places.
 
+While it is generally recommended to use the `greek` module for your
+decoding needs, which is based on unicode composition and is generally
+faster, the `old_greek` module, which uses a prefix tree, will catch
+certain types of betacode errors that the new decoder does not, which
+can be helpful if you suspect you are dealing with bad betacode.
 '''
 from betacode import greek, hebrew, old_greek
